1506122074d75e022d33795cbacd053da377b34e
jesus <jesusalbertoramirezlopez.jr@gmail.com>
2020-06-22 15:02:52 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // BASE DE DATOS
 let urlDB;
 if(process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/cafe';
-else urlDB = 'mongodb+srv://usuario1:1dBQt78TgW2lOGgW@cluster0-pj2tn.mongodb.net/cafe';
+else urlDB = process.env.MONGO_URI;
 
 // urlDB = 'mongodb+srv://usuario1:1dBQt78TgW2lOGgW@cluster0-pj2tn.mongodb.net/cafe';
 process.env.URLDB = urlDB;
